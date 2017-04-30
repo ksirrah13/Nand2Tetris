5fd0ad7a3042b707df6c06088735d02ddd2aeb4c
kyle.l.harris <kyle.l.harris@workday.com>
2017-04-29 23:17:17 -0700

    chapter 10 - syntax analyzer without look ahead for terms

--- 10/JackTranslator/CompilationEngine.java
+++ 10/JackTranslator/CompilationEngine.java
@@ -49,12 +49,12 @@ class CompilationEngine {
         }
     }
 
-    private void validateTokenType(JackTokenizer.TokenIdentifier id) {
+    private void validateSymbols(List<String> symbols) {
         tokenizer.advance();
-        if (tokenizer.tokenType() != id) {
-            throw new IllegalArgumentException("Syntax Error: Expected type " + id.getXmlName() + " but found " + tokenizer.tokenType().getXmlName());
-        }
-    }
+        if (tokenizer.tokenType() != JackTokenizer.TokenIdentifier.SYMBOL ||
+                !symbols.contains(tokenizer.symbol())) {
+            throw new IllegalArgumentException("Syntax Error: Expected one of symbols " + symbols.toString() + " but found " + tokenizer.symbol());
+        }    }
 
     private void validateSymbol(String symbol) {
         tokenizer.advance();
@@ -93,6 +93,57 @@ class CompilationEngine {
                 tokenizer.keyword().equals("void");
     }
 
+    private void validateStatement() {
+        tokenizer.advance();
+        if (!isStatement()) {
+            throw new IllegalArgumentException("Syntax Error: Expected let, if , while, do or return");
+        }
+    }
+
+    private boolean isStatement() {
+        return tokenizer.tokenType() == JackTokenizer.TokenIdentifier.KEYWORD &&
+                Arrays.asList("let", "if", "while", "do", "return").contains(tokenizer.keyword());
+    }
+
+    private void validateTerm() {
+        tokenizer.advance();
+        if (!isTerm()) {
+            throw new IllegalArgumentException("Syntax Error: Expected term");
+        }
+    }
+
+    private boolean isTerm() {
+        return isKeywordConstant() || isUnaryOp() || isOpenParen() ||
+                tokenizer.tokenType() == JackTokenizer.TokenIdentifier.IDENTIFIER ||
+                tokenizer.tokenType() == JackTokenizer.TokenIdentifier.INT_CONST ||
+                tokenizer.tokenType() == JackTokenizer.TokenIdentifier.STRING_CONST;
+    }
+
+    private boolean isComma() {
+        return tokenizer.tokenType() == JackTokenizer.TokenIdentifier.SYMBOL &&
+                tokenizer.symbol().equals(",");
+    }
+
+    private boolean isKeywordConstant() {
+        return tokenizer.tokenType() == JackTokenizer.TokenIdentifier.KEYWORD &&
+                Arrays.asList("true", "false", "null", "this").contains(tokenizer.keyword());
+    }
+
+    private boolean isUnaryOp() {
+        return tokenizer.tokenType() == JackTokenizer.TokenIdentifier.SYMBOL &&
+                Arrays.asList("-", "~").contains(tokenizer.symbol());
+    }
+
+    private boolean isOp() {
+        return tokenizer.tokenType() == JackTokenizer.TokenIdentifier.SYMBOL &&
+                Arrays.asList("+", "-", "*", "/", "&", "|", "<", ">", "=").contains(tokenizer.symbol());
+    }
+
+    private boolean isOpenParen() {
+        return tokenizer.tokenType() == JackTokenizer.TokenIdentifier.SYMBOL &&
+                tokenizer.symbol().equals("(");
+    }
+
     List<String> compile() {
             validateKeyword("class");
             return compileClass();
@@ -124,12 +175,10 @@ class CompilationEngine {
             tokenizer.advance();
         }
 
-
+        validateSymbolNoAdvance("}");
 
         output.add(end("class"));
-
         return output;
-
     }
 
     private List<String> compileClassVarDec() {
@@ -154,7 +203,7 @@ class CompilationEngine {
             tokenizer.advance();
         }
 
-        validateSymbol(";");
+        validateSymbolNoAdvance(";");
         output.add(wrap(tokenizer.symbol()));
 
         output.add(end("classVarDec"));
@@ -181,7 +230,7 @@ class CompilationEngine {
         validateSymbolNoAdvance(")");
         output.add(wrap(tokenizer.symbol()));
 
-        tokenizer.advance();
+        validateSymbol("{");
         output.addAll(compileSubroutineBody());
 
         return output;
@@ -198,7 +247,7 @@ class CompilationEngine {
             output.add(wrap(tokenizer.identifier()));
 
             tokenizer.advance();
-            while(tokenizer.tokenType() == JackTokenizer.TokenIdentifier.SYMBOL &&
+            while (tokenizer.tokenType() == JackTokenizer.TokenIdentifier.SYMBOL &&
                     tokenizer.symbol().equals(",")) {
                 output.add(wrap(tokenizer.symbol()));
 
@@ -206,7 +255,7 @@ class CompilationEngine {
                 output.add(wrappedType());
 
                 validateIdentifier();
-                output.add(tokenizer.identifier());
+                output.add(wrap(tokenizer.identifier()));
 
                 tokenizer.advance();
             }
@@ -219,10 +268,331 @@ class CompilationEngine {
     private List<String> compileSubroutineBody() {
         List<String> output = new ArrayList<>();
         output.add(start("subroutineBody"));
+        output.add(wrap(tokenizer.symbol()));
+
+        tokenizer.advance();
+        while (tokenizer.tokenType() == JackTokenizer.TokenIdentifier.KEYWORD &&
+                tokenizer.keyword().equals("var")) {
+            output.addAll(compileVarDec());
+
+            tokenizer.advance();
+        }
+
+        if (isStatement()) {
+            output.addAll(compileStatements());
+        }
+
+        validateSymbolNoAdvance("}");
+        output.add(wrap(tokenizer.symbol()));
 
         output.add(end("subroutineBody"));
+        return output;
+    }
+
+    private List<String> compileVarDec() {
+        List<String> output = new ArrayList<>();
+        output.add(start("varDec"));
+        output.add(wrap(tokenizer.keyword()));
+
+        validateType();
+        output.add(wrappedType());
+
+        validateIdentifier();
+        output.add(wrap(tokenizer.identifier()));
+
+        tokenizer.advance();
+        while (isComma()) {
+            output.add(wrap(tokenizer.symbol()));
+
+            validateIdentifier();
+            output.add(wrap(tokenizer.identifier()));
+
+            tokenizer.advance();
+        }
+
+        validateSymbolNoAdvance(";");
+        output.add(wrap(tokenizer.symbol()));
+
+        output.add(end("varDec"));
+        return output;
+    }
+
+    private List<String> compileStatements() {
+        List<String> output = new ArrayList<>();
+        output.add(start("statements"));
+
+        while (isStatement()) {
+            switch (tokenizer.keyword()) {
+                case "let": {
+                    output.addAll(compileLetStatement());
+                    tokenizer.advance();
+                    break;
+                }
+                case "if": {
+                    output.addAll(compileIfStatement());
+                    break;
+                }
+                case "while": {
+                    output.addAll(compileWhileStatement());
+                    tokenizer.advance();
+                    break;
+                }
+                case "do": {
+                    output.addAll(compileDoStatement());
+                    tokenizer.advance();
+                    break;
+                }
+                case "return": {
+                    output.addAll(compileReturnStatement());
+                    tokenizer.advance();
+                    break;
+                }
+            }
+        }
+
+        output.add(end("statements"));
+        return output;
+    }
+
+    private List<String> compileLetStatement() {
+        List<String> output = new ArrayList<>();
+        output.add(start("letStatement"));
+        output.add(wrap(tokenizer.keyword()));
+
+        validateIdentifier();
+        output.add(wrap(tokenizer.identifier()));
+
+        tokenizer.advance();
+        if (tokenizer.tokenType() == JackTokenizer.TokenIdentifier.SYMBOL &&
+                tokenizer.symbol().equals("[")) {
+            output.add(wrap(tokenizer.symbol()));
+
+            validateTerm();
+            output.addAll(compileExpression());
+
+            validateSymbolNoAdvance("]");
+            output.add(wrap(tokenizer.symbol()));
+
+            tokenizer.advance();
+        }
+
+        validateSymbolNoAdvance("=");
+        output.add(wrap(tokenizer.symbol()));
+
+        validateTerm();
+        output.addAll(compileExpression());
+
+        validateSymbolNoAdvance(";");
+        output.add(wrap(tokenizer.symbol()));
+
+        output.add(end("letStatement"));
+        return output;
+    }
+
+    private List<String> compileExpression() {
+        List<String> output = new ArrayList<>();
+        output.add(start("expression"));
+        output.addAll(compileTerm());
+
+        tokenizer.advance();
+        while (isOp()) {
+            output.add(wrap(tokenizer.symbol()));
+
+            validateTerm();
+            output.addAll(compileTerm());
+
+            tokenizer.advance();
+        }
+
+        output.add(end("expression"));
+        return output;
+    }
+
+    private List<String> compileTerm() {
+        List<String> output = new ArrayList<>();
+        output.add(start("term"));
+
+        if (tokenizer.tokenType() == JackTokenizer.TokenIdentifier.INT_CONST) {
+            output.add(wrap(tokenizer.intVal()));
+        } else if (tokenizer.tokenType() == JackTokenizer.TokenIdentifier.STRING_CONST) {
+            output.add(wrap(tokenizer.stringVal()));
+        } else if (isKeywordConstant()) {
+            output.add(wrap(tokenizer.keyword()));
+        } else if (isOpenParen()) {
+            output.add(wrap(tokenizer.symbol()));
+
+            validateTerm();
+            output.addAll(compileExpression());
+
+            validateSymbolNoAdvance(")");
+        } else if (isUnaryOp()) {
+            output.add(wrap(tokenizer.symbol()));
+
+            validateTerm();
+            output.addAll(compileTerm());
+        } else if (tokenizer.tokenType() == JackTokenizer.TokenIdentifier.IDENTIFIER) {
+
+            //todo look ahead
+
+        }
+        output.add(end("term"));
+        return output;
+    }
+
+    private List<String> compileIfStatement() {
+        List<String> output = new ArrayList<>();
+        output.add(start("ifStatement"));
+
+        output.add(wrap(tokenizer.keyword()));
+
+        output.addAll(compileConditionBlock());
+
+        tokenizer.advance();
+        if (tokenizer.tokenType() == JackTokenizer.TokenIdentifier.KEYWORD &&
+                tokenizer.keyword().equals("else")) {
+            output.add(wrap(tokenizer.keyword()));
+
+            validateSymbol("{");
+            output.add(tokenizer.symbol());
+
+            validateStatement();
+            output.addAll(compileStatements());
+
+            validateSymbolNoAdvance("}");
+
+            tokenizer.advance();
+        }
+
+        output.add(end("ifStatement"));
+        return output;
+    }
+
+    private List<String> compileWhileStatement() {
+        List<String> output = new ArrayList<>();
+        output.add(start("whileStatement"));
+
+        output.add(wrap(tokenizer.keyword()));
+
+        output.addAll(compileConditionBlock());
+
+        output.add(end("whileStatement"));
+        return output;
+    }
+
+    private List<String> compileConditionBlock() {
+        List<String> output = new ArrayList<>();
+
+        validateSymbol("(");
+        output.add(wrap(tokenizer.symbol()));
+
+        validateTerm();
+        output.addAll(compileExpression());
+
+        validateSymbol(")");
+        output.add(wrap(tokenizer.symbol()));
+
+        validateSymbol("{");
+        output.add(wrap(tokenizer.symbol()));
+
+        validateStatement();
+        output.addAll(compileStatements());
+
+        validateSymbolNoAdvance("}");
+        output.add(wrap(tokenizer.symbol()));
 
         return output;
-
     }
+
+    private List<String> compileDoStatement() {
+        List<String> output = new ArrayList<>();
+        output.add(start("doStatement"));
+
+        output.add(wrap(tokenizer.keyword()));
+
+        validateIdentifier();
+        output.addAll(compileSubroutineCall());
+
+        validateSymbol(";");
+        output.add(wrap(tokenizer.symbol()));
+
+        output.add(end("doStatement"));
+        return output;
+    }
+
+    private List<String> compileReturnStatement() {
+        List<String> output = new ArrayList<>();
+        output.add(start("returnStatement"));
+
+        output.add(wrap(tokenizer.keyword()));
+
+        tokenizer.advance();
+        if (isTerm()) {
+            output.addAll(compileExpression());
+        }
+
+        validateSymbolNoAdvance(";");
+        output.add(wrap(tokenizer.symbol()));
+
+        output.add(end("returnStatement"));
+        return output;
+    }
+
+    private List<String> compileSubroutineCall() {
+        List<String> output = new ArrayList<>();
+        output.add(start("subroutineCall"));
+
+        output.add(wrap(tokenizer.identifier()));
+
+        validateSymbols(Arrays.asList(".", "("));
+        if (tokenizer.symbol().equals("(")) {
+            output.add(wrap(tokenizer.symbol()));
+
+            output.addAll(compileExpressionList());
+
+            validateSymbolNoAdvance(")");
+            output.add(wrap(tokenizer.symbol()));
+        } else {
+            output.add(wrap(tokenizer.symbol()));
+
+            validateIdentifier();
+            output.add(wrap(tokenizer.identifier()));
+
+            validateSymbol("(");
+            output.add(wrap(tokenizer.symbol()));
+
+            output.addAll(compileExpressionList());
+
+            validateSymbolNoAdvance(")");
+            output.add(wrap(tokenizer.symbol()));
+        }
+
+        output.add(end("subroutineCall"));
+        return output;
+    }
+
+    private List<String> compileExpressionList() {
+        List<String> output = new ArrayList<>();
+        tokenizer.advance();
+
+        if (isTerm()) {
+            output.add(start("expressionList"));
+
+            output.addAll(compileExpression());
+
+            tokenizer.advance();
+            while (isComma()) {
+                output.add(wrap(tokenizer.symbol()));
+
+                validateTerm();
+                output.addAll(compileExpression());
+
+                tokenizer.advance();
+            }
+
+            output.add(start("expressionList"));
+        }
+
+        return output;
+    }
+
 }
